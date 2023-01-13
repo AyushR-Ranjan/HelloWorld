c96f01442fe3dffefd5b67ec1d8655e2b74a4867
AyushR-Ranjan <122555382+AyushR-Ranjan@users.noreply.github.com>
2023-01-13 12:25:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.No)
+    music.stopAllSounds()
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+    led.enable(Math.randomBoolean())
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Yes)
+    music.playMelody("E B C5 A B G A F ", 500)
 })
 basic.showIcon(IconNames.Happy)
