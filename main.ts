87831ba9ae503b49ff1bbfb47548d02a5b976d63
AyushR-Ranjan <122555382+AyushR-Ranjan@users.noreply.github.com>
2023-01-13 12:06:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.No)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Yes)
 })
-basic.showIcon(IconNames.No)
+basic.showIcon(IconNames.Happy)
